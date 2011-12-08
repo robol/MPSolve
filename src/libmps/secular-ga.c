79b8b4256f4dea3607ff109e9c3344a0b02b1327
Leonardo Robol <leo@robol.it>
2011-12-08 14:56:19 +0100

    Rimesso DBL_MAX come raggio iniziale per evitare overflow.

--- src/libmps/secular-ga.c
+++ src/libmps/secular-ga.c
@@ -342,7 +342,8 @@ mps_secular_ga_mpsolve (mps_status * s)
   for (i = 0; i < s->n; i++)
     {
       s->frad[i] = DBL_MAX;
-      rdpe_set (s->drad[i], RDPE_MAX);
+      // rdpe_set (s->drad[i], RDPE_MAX);
+      rdpe_set_d (s->drad[i], DBL_MAX);
     }
   
   /* Set initial cluster structure as no cluster structure. */
