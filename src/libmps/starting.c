493cb798cef437860d6207ca07b57c40ab023188
Leonardo Robol <leo@robol.it>
2012-04-24 16:45:17 +0200

    Aggiunto altro workaround nello shift.

--- src/libmps/starting.c
+++ src/libmps/starting.c
@@ -1015,6 +1015,8 @@ mps_mstart (mps_status * s, int n, mps_cluster_item * cluster_item,
        * and set the corresponding radius */
       rdpe_set (rtmp1, s->dradii[i]);
       rdpe_mul_eq_d (rtmp1, (double) nzeros);
+      if (rdpe_lt (rtmp1, clust_rad))
+	rdpe_set (rtmp1, clust_rad);
       rdpe_set (rtmp2, g);
       rdpe_mul_eq (rtmp2, s->eps_out);
       MPS_DEBUG (s, "Performing relatively small check");
