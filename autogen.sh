f27ce50dffb8144c277161da5ad3ecc3d675013a
Leonardo Robol <leo@robol.it>
2012-12-05 20:34:06 +0100

    Updated autogen.sh with some checks to improve its robustness.

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Autoconfiguratio script for MPSolve.
+# Autoconfiguration script for MPSolve.
 #
 # Author: Leonardo Robol <leo@robol.it>
 
@@ -8,4 +8,18 @@ if [ ! -d "m4" ]; then
   mkdir m4
 fi
 
-autoreconf -i --force || exit 1
+AUTORECONF=$(which autoreconf)
+if [ $? -ne 0 ]; then
+  echo "It appears that Autotools is not correctly installed on this system."
+  echo "Please install it and re-run this script."
+  exit 1
+fi
+
+which libtool > /dev/null
+if [ $? -ne 0 ]; then
+  echo "It appears that Libtool is not correctly installed on this system."
+  echo "Please install it and re-run this script."
+  exit 2
+fi
+
+$AUTORECONF -i --force || exit 3
