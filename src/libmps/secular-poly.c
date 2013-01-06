bd26cd9664d53f5cb1f3abcd578f6d2f8081c1e4
Leonardo Robol <leo@robol.it>
2013-01-06 12:08:21 +0100

    Added missing C files.

---
+++ src/libmps/secular-poly.c
@@ -0,0 +1,16 @@
+/*
+ * This file is part of MPSolve 3.0
+ *
+ * Copyright (C) 2001-2012, Dipartimento di Matematica "L. Tonelli", Pisa.
+ * License: http://www.gnu.org/licenses/gpl.html GPL version 3 or higher
+ *
+ * Authors: 
+ *   Dario Andrea Bini <bini@dm.unipi.it>
+ *   Giuseppe Fiorentino <fiorent@dm.unipi.it>
+ *   Leonardo Robol <robol@mail.dm.unipi.it>
+ */
+
+#include <mps/secular-poly.h>
+#include <mps/mps.h>
+
+
