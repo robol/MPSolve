a0e1e2449f9692b76bc535a2877267275424a3f9
Leonardo Robol <leo@robol.it>
2013-03-07 16:22:17 +0100

    Remove useless workaround.

--- src/libmps/secular-ga.c
+++ src/libmps/secular-ga.c
@@ -417,7 +417,7 @@ mps_secular_ga_mpsolve (mps_context * s)
 
        /* Check if all the roots are approximated or, if we have done more than 4 packets
         * of iterations without finding all of them, if at least we are near to the result. */
-       if (roots_computed >= MIN (s->n, s->n + 4 - packet) || true)
+       if (roots_computed >= MIN (s->n, s->n + 4 - packet))
          {
            if (mps_secular_ga_regenerate_coefficients (s))
              {
