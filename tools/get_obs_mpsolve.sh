a4757449092b92c44f4e90d0264aa3f08ef22a66
Leonardo Robol <leo@robol.it>
2013-10-19 14:25:30 +0200

    Refined th einstaller for Windows. Added a script to automate the building.

---
+++ tools/get_obs_mpsolve.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+
+if [ -d "windows-mpsolve" ]; then
+  rm -r windows-mpsolve
+fi
+
+mkdir windows-mpsolve
+cd windows-mpsolve
+
+cp ../download-mingw-rpm.py .
+
+./download-mingw-rpm.py --no-clean --deps mingw32-libqt4 mingw32-libgmp mingw32-pthreads
+./download-mingw-rpm.py --no-deps --no-clean -p home:lrobol mpsolve
+
+mv  usr/i686-w64-mingw32/sys-root/mingw/bin MPSolve
+rm -r usr/
+rm -r cache/
+
+cd MPSolve
+
+cp ../../installer.nsis .
+makensis installer.nsis
+
+
