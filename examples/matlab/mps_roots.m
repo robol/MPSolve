f8a38da139911aca5d0fdf3adbe2fea71daf89f0
Leonardo Robol <leo@robol.it>
2013-12-05 16:40:17 +0100

    Added documentation for the Matlab (tm) toolbox.

---
+++ examples/matlab/mps_roots.m
@@ -0,0 +1,11 @@
+%% MPS_ROOTS function that approximate the roots of a polynomial specified
+%% by the vector v using MPSolve and the algorithm specified by alg. 
+%% alg can be 's' or 'u', depending if the algorithm MPS_ALGORITHM_SECULAR_GA or
+%% the algorithm MPS_ALGORITHM_SECULAR_UNISOLVE are desired. 
+%%
+%% Author: Leonardo Robol <leonardo.robol@sns.it>
+%% Copyright: 2011-2013 Leonardo Robol <leonardo.robol@sns.it>
+%% License: GPLv3 or higher
+function x = mps_roots(v, alg)
+  error("MEX file not found. Aborting"); 
+end
