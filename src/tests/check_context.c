d5991f961d9e7176e2fdaa8cf77f18050ecd749f
Leonardo Robol <leo@robol.it>
2013-12-20 07:54:15 +0100

    Added a test for in process context reuse.

--- src/tests/check_context.c
+++ src/tests/check_context.c
@@ -9,6 +9,30 @@ START_TEST (basics_allocate_context)
 }
 END_TEST
 
+START_TEST (basics_context_reuse_without_free)
+{
+	mps_context * ctx = mps_context_new ();
+
+	/* Start with a simple polynomial of degree 2 */
+	mps_monomial_poly *poly = mps_monomial_poly_new (ctx, 2);
+
+	mps_monomial_poly_set_coefficient_d (ctx, poly, 0, -1, 0.0);
+	mps_monomial_poly_set_coefficient_d (ctx, poly, 2, 1, 0.0);
+
+	mps_context_set_input_poly (ctx, MPS_POLYNOMIAL (poly));
+	mps_mpsolve (ctx);
+
+	/* Then try to load a polynomial with a higher degree */
+	poly = mps_monomial_poly_new (ctx, 10);
+	mps_monomial_poly_set_coefficient_d (ctx, poly, 0, -1, 0.0);
+	mps_monomial_poly_set_coefficient_d (ctx, poly, 10, 1, 0.0);
+
+	mps_context_set_input_poly (ctx, MPS_POLYNOMIAL (poly));
+	mps_mpsolve (ctx);
+	mps_monomial_poly_free (ctx, MPS_POLYNOMIAL (poly));
+}
+END_TEST
+
 START_TEST (basics_context_reuse_expand)
 {
 	mps_context * ctx = mps_context_new ();
@@ -71,6 +95,7 @@ main (void)
 
   // Basic operation
   tcase_add_test (tc_basics, basics_allocate_context);
+  tcase_add_test (tc_basics, basics_context_reuse_without_free);
   tcase_add_test (tc_basics, basics_context_reuse_expand);
   tcase_add_test (tc_basics, basics_context_reuse_shrink);
 
