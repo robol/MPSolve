b3dc564cf3b795064baa41dcaff79699cbfc0772
Leonardo Robol <leo@robol.it>
2014-04-30 15:01:34 +0200

    Tune the perturbations for the starting points of mandelbrot polynomials.

--- examples/mandelbrot-solver/mandelbrot-starting.c
+++ examples/mandelbrot-solver/mandelbrot-starting.c
@@ -65,7 +65,7 @@ mps_mandelbrot_poly_fstart (mps_context *ctx, mps_polynomial *p, mps_approximati
   mps_mandelbrot_poly *mp = MPS_MANDELBROT_POLY (p);
   int n = mps_context_get_degree (ctx);
 
-  if (mp->level <= 5)
+  if (mp->level <= 7)
     {
       mps_general_fstart (ctx, p, approximations);
     }
@@ -73,15 +73,19 @@ mps_mandelbrot_poly_fstart (mps_context *ctx, mps_polynomial *p, mps_approximati
     {
       int i;
       cplx_t *roots = cplx_valloc (p->degree / 2);
-      cplx_t rot;
+      cplx_t rot_up, rot_down;
 
-      cplx_set_d (rot, 1 + p->degree * DBL_EPSILON, 0.0);
+      int eps = (mp->level > DBL_DIG) ? mp->level : DBL_DIG + 1;
+
+      cplx_set_d (rot_up, 1 + pow (.5, eps), -4.0 * DBL_EPSILON);
+      cplx_set_d (rot_down, 1 + pow (.5, eps), 4.0 * DBL_EPSILON);
 
       mps_context * new_ctx = mps_context_new ();
       mps_mandelbrot_poly *new_mp = mps_mandelbrot_poly_new (new_ctx, mp->level - 1);
 
       mps_context_set_input_poly (new_ctx, MPS_POLYNOMIAL (new_mp));
       mps_context_select_algorithm (new_ctx, MPS_ALGORITHM_SECULAR_GA);
+      mps_context_set_output_prec (new_ctx, 16);
       mps_mpsolve (new_ctx);
 
       mps_context_get_roots_d (new_ctx, &roots, NULL);
@@ -93,13 +97,21 @@ mps_mandelbrot_poly_fstart (mps_context *ctx, mps_polynomial *p, mps_approximati
 	  cplx_set (starting_point, roots[j]);
 
 	  if (i % 2 == 0)
-	    cplx_mul_eq (starting_point, rot);
+	    cplx_mul_eq (starting_point, (cplx_Im (roots[j]) > 0) ? rot_up : rot_down);
+	  else
+	    cplx_div_eq (starting_point, (cplx_Im (roots[j]) > 0) ? rot_up : rot_down);
 
 	  mps_approximation_set_fvalue (ctx, approximations[i], starting_point);
 	  mps_approximation_set_frad (ctx, approximations[i], DBL_MAX);
 	}
 
-      mps_approximation_set_fvalue (ctx, approximations[i], cplx_one);
+      if (mp->level % 2 == 1)
+	{
+	  cplx_set_d (rot_up, -1.0, 0.0);
+	  mps_approximation_set_fvalue (ctx, approximations[i], rot_up);
+	}
+      else
+	mps_approximation_set_fvalue (ctx, approximations[i], cplx_one);
 
       cplx_vfree (roots);
       mps_mandelbrot_poly_free (new_ctx, MPS_POLYNOMIAL (new_mp));
