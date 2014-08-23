57a35f8cb869fd06327f020af705da950ad19228
Leonardo Robol <leo@robol.it>
2014-08-23 10:41:59 +0200

    Added load_mpsolve.m

---
+++ examples/matlab/load_mpsolve.m
@@ -0,0 +1,4 @@
+% Load the M-files and MEX files contained in the mpsolve-toolbox
+% folder. This file should be called in the same directory where
+% the mpsolve-toolbox folder is placed. 
+addpath('mpsolve-toolbox');
