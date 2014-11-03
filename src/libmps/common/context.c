81216dddf14b4ee348d5a32b0661ebbceeb247f6
Leonardo Robol <leo@robol.it>
2014-11-03 16:10:53 +0100

    Corrected typo.

--- src/libmps/common/context.c
+++ src/libmps/common/context.c
@@ -16,10 +16,10 @@
 #include <string.h>
 
 static mps_context ** context_factory = NULL;
-static int context_factory_size = 1;
+static int context_factory_size = 0;
 static pthread_mutex_t context_factory_mutex = PTHREAD_MUTEX_INITIALIZER;
 
-#define MPS_CONTEXT_FACTORY_MAXIMUM_SIZE 0
+#define MPS_CONTEXT_FACTORY_MAXIMUM_SIZE 1
 
 long int
 mps_context_get_minimum_precision (mps_context * s)
