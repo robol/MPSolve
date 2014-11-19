4e915b71f0b1c18c7f8aa760eebf9726d0c1dba2
Leonardo Robol <leo@robol.it>
2014-11-19 16:56:20 +0100

    Fix autogen.sh.

--- autogen.sh
+++ autogen.sh
@@ -25,11 +25,4 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-which libtool > /dev/null
-if [ $? -ne 0 ]; then
-  echo "It appears that Libtool is not correctly installed on this system."
-  echo "Please install it and re-run this script."
-  exit 2
-fi
-
 $AUTORECONF -i --force || exit 3
