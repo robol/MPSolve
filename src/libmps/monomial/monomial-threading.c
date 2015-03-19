7e4b1157ebbd55b5aedb383ce1e323eab9f257a7
Leonardo Robol <leo@robol.it>
2015-03-19 22:53:58 +0100

    Updated copyright statement.

--- src/libmps/monomial/monomial-threading.c
+++ src/libmps/monomial/monomial-threading.c
@@ -1,7 +1,7 @@
 /*
  * This file is part of MPSolve 3.1.5
  *
- * Copyright (C) 2001-2014, Dipartimento di Matematica "L. Tonelli", Pisa.
+ * Copyright (C) 2001-2015, Dipartimento di Matematica "L. Tonelli", Pisa.
  * License: http://www.gnu.org/licenses/gpl.html GPL version 3 or higher
  *
  * Authors:
