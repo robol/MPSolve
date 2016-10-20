c34b10e4faec892df42cd1fe2875f0be59be06be
Leonardo Robol <leo@robol.it>
2016-10-20 21:15:38 +0200

    Refactored the loading of libraries in MEX files.

--- examples/matlab/load_mpsolve.m
+++
@@ -1,4 +0,0 @@
-% Load the M-files and MEX files contained in the mpsolve-toolbox
-% folder. This file should be called in the same directory where
-% the mpsolve-toolbox folder is placed. 
-addpath('mpsolve-toolbox');
