f08d6d6676304c63b6ae2810f339481565c3387f
Leonardo Robol <leo@robol.it>
2017-02-09 09:23:38 +0100

    Expanded CMake configuration with examples and installation.

---
+++ examples/python/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name="MPSolve",
+      version="1.0",
+      description="Python interface to MPSolve",
+      author="Leonardo Robol",
+      author_email="leo@robol.it",
+      packages=[ 'mpsolve' ])
+      
