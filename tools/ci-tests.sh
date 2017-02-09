b047990affd75f6b9ee50610ec99321dd63e24a6
Leonardo Robol <leo@robol.it>
2017-02-09 09:42:55 +0100

    Updated CMake configuration.

--- tools/ci-tests.sh
+++ tools/ci-tests.sh
@@ -5,16 +5,16 @@
 # Test CMake build
 mkdir cmake-build
 cd cmake-build/
-cmake ..
-make VERBOSE=1
-make test
+cmake .. || exit 1
+make VERBOSE=1 || exit 1
+make test || exit 1
 cd ..
 
 # Test autotools build
 ./autogen.sh
 mkdir build
 cd build/
-../configure MEX=mkoctfile MEXTOPS='--mex'
-make V=1
-make check V=1
+../configure MEX=mkoctfile MEXTOPS='--mex' || exit 1
+make V=1 || exit 1
+make check V=1 || exit 1
 
