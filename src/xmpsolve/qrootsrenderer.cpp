8e3e8f94a896a436f640be2e180d43cb4ba294b7
Leonardo Robol <leo@robol.it>
2019-10-01 12:09:30 +0200

    Made QRootsRenderer QT4 compatible.

--- src/xmpsolve/qrootsrenderer.cpp
+++ src/xmpsolve/qrootsrenderer.cpp
@@ -55,7 +55,11 @@ QRootsRenderer::mousePressEvent(QMouseEvent *event)
 {
     if (event->button() == Qt::LeftButton) {
         mDragging = true;
+#if QT_VERSION >= 0x050000
         mPreviousPosition = event->localPos();
+#else
+	mPreviousPosition = event->posF();
+#endif
     }
     QWidget::mousePressEvent(event);
 }
@@ -73,10 +77,18 @@ void
 QRootsRenderer::mouseMoveEvent(QMouseEvent *event)
 {
     if (mDragging) {
+#if QT_VERSION >= 0x050000
         QPointF distance = event->localPos() - mPreviousPosition;
+#else
+	QPointF distance = event->posF() - mPreviousPosition;
+#endif
         distance = scaleVectorInverse(distance, width(), height());
         setCenter(center().x() - distance.x(), center().y() - distance.y());
+#if QT_VERSION >= 0x050000
         mPreviousPosition = event->localPos();
+#else
+	mPreviousPosition = event->posF();
+#endif
     }
     QWidget::mouseMoveEvent(event);
 }
